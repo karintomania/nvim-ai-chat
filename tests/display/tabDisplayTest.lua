502274601f1bb66f74e9e4c64dc952173c41829a
karintomania <19652340+karintomania@users.noreply.github.com>
2022-12-23 21:36:56 +0000

    add tabDisplay

---
+++ tests/display/tabDisplayTest.lua
@@ -0,0 +1,9 @@
+package.loaded['lua/nvim-ai-chat/display/tabDisplay'] = nil
+local tabDisplay = require('lua/nvim-ai-chat/display/tabDisplay')
+
+package.loaded['lua/nvim-ai-chat/display/resultBuffer'] = nil
+local resultBuffer = require('lua/nvim-ai-chat/display/resultBuffer')
+
+local handle = resultBuffer.create('test question' ,"test result\nsecond line")
+
+tabDisplay.display(buffer)
