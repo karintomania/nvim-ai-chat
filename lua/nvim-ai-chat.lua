6418cfe4925a2407e41e98882fc09a8ef0c437e7
karintomania <19652340+karintomania@users.noreply.github.com>
2023-08-07 14:32:00 +0100

    remove old implementation

--- lua/nvim-ai-chat.lua
+++ lua/nvim-ai-chat.lua
@@ -1,10 +1,5 @@
 local M = {}
 
-local api = require('nvim-ai-chat/api/api')
-local client = require('nvim-ai-chat/api/curlClient')
-local resultBuffer = require('nvim-ai-chat/display/resultBuffer')
-local display = require('nvim-ai-chat/display/tabDisplay')
-
 require('nvim-ai-chat/display/ChatManager')
 local chatManager = ChatManager:new()
 require('nvim-ai-chat/display/InputManager')
@@ -21,24 +16,6 @@ M.config = {
 function M.setup(customConfig)
 	local filled = vim.tbl_deep_extend("keep", customConfig, M.config)
 	M.config = filled
-end
-
-function M.chat(question)
-	local answer = api.call(question, client, M.config)
-
-	local buffer = resultBuffer.create(question, answer)
-	
-	display.display(buffer)
-end
-
-function M.chatSelection(lineStart, lineEnd, additionalQuestion)
-    local lines = vim.fn.getline(lineStart, lineEnd)
-    local question = table.concat(lines, "\n")
-
-    if additionalQuestion ~= nil and additionalQuestion ~= ""  then
-        question = additionalQuestion .. "\n" .. question
-    end
-    M.chat(question)
 end
 
 function M.ask()
--- tests/testSuite.lua
+++ tests/testSuite.lua
@@ -7,15 +7,6 @@
 ----------------
 -- API tests
 ----------------
-package.loaded['tests/api/apiTest'] = nil
-require('tests/api/apiTest')
-
--- package.loaded['tests/api/curlClientTest'] = nil
--- require('tests/api/curlClientTest')
-
-package.loaded['tests/api/curlJsonResponseTest'] = nil
-require('tests/api/curlJsonResponseTest')
-
 package.loaded['tests/api/chatCurlClientTest'] = nil
 require('tests/api/chatCurlClientTest')
 
@@ -23,9 +14,6 @@ require('tests/api/chatCurlClientTest')
 ----------------
 -- Display tests
 ----------------
-package.loaded['tests/display/resultBufferTest'] = nil
-require('tests/display/resultBufferTest')
-
 package.loaded['tests/display/BufferTest'] = nil
 require('tests/display/BufferTest')
 
@@ -36,8 +24,8 @@ package.loaded['tests/display/InputManagerTest'] = nil
 require('tests/display/InputManagerTest')
 
 -- these are skipped as it requires manual checks
--- package.loaded['tests/display/tabDisplayTest'] = nil
--- require('tests/display/tabDisplayTest')
+-- package.loaded['tests/display/openTabTest'] = nil
+-- require('tests/display/openTabTest'')
 
 ----------------
 -- Util tests
